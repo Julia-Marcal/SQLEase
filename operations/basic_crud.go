019b01e997a889d642c225401a9e8721ff860160
Julia-Marcal <anajulia.05marcal@gmail.com>
2024-01-07 14:13:54 -0300

    make insertion work with various values

--- operations/basic_crud.go
+++ operations/basic_crud.go
@@ -2,22 +2,38 @@ package operations
 
 import (
 	"database/sql"
+	"fmt"
+	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func Create(db *sql.DB, table string, column string, value string) error {
-	_, err := db.Exec("INSERT INTO "+table+" ("+column+") VALUES (?)", value)
+func Create(db *sql.DB, table string, columns []string, values []string) error {
+	if len(columns) != len(values) {
+		return fmt.Errorf("the number of columns and values do not match")
+	}
+
+	placeholders := strings.Repeat("?,", len(values))
+	placeholders = placeholders[:len(placeholders)-1]
+
+	query := fmt.Sprintf("INSERT INTO %s (%s) VALUES (%s)", table, strings.Join(columns, ", "), placeholders)
+
+	interfaceValues := make([]interface{}, len(values))
+	for i, v := range values {
+		interfaceValues[i] = v
+	}
+
+	_, err := db.Exec(query, interfaceValues...)
 	return err
 }
 
 func Read(db *sql.DB, id int, column string, table string) (string, string, error) {
-	var username, email string
-	err := db.QueryRow("SELECT "+column+" FROM "+table+" WHERE id = ?", id).Scan(&username, &email)
+	var name, age string
+	err := db.QueryRow("SELECT "+column+" FROM "+table+" WHERE id = ?", id).Scan(&name, &age)
 	if err != nil {
 		return "", "", err
 	}
-	return username, email, nil
+	return name, age, nil
 }
 
 func Update(db *sql.DB, table string, column string, id int, newValue string) error {
